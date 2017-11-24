ec8884c4c58391000a63772ff788149f70e126ef
Ryan Hartlage <ryanplusplus@gmail.com>
2017-11-24 09:11:47 -0500

    Publish to lit in a non-stupid way

---
+++ package.lua
@@ -0,0 +1,14 @@
+  return {
+    name = "ryanplusplus/mach",
+    version = "1.0.15",
+    description = "Simple mocking framework for Lua inspired by CppUMock and designed for readability",
+    tags = { "testing" },
+    license = "MIT",
+    author = { name = "Ryan Hartlage", email = "ryanplusplus@gmail.com" },
+    homepage = "https://github.com/ryanplusplus/mach.lua",
+    dependencies = {},
+    files = {
+      "**.lua",
+      "!spec*"
+    }
+  }
