7bffe77ba475a0dad2f1d25b08a9a902d4d8d315
Ryan Hartlage <ryanplusplus@gmail.com>
2019-02-14 21:40:41 -0500

    Exorcised colons from Expectation

--- src/mach/Expectation.lua
+++ src/mach/Expectation.lua
@@ -5,198 +5,203 @@ local unexpected_args_error = require 'mach.unexpected_args_error'
 local out_of_order_call_error = require 'mach.out_of_order_call_error'
 local not_all_calls_occurred_error = require 'mach.not_all_calls_occurred_error'
 
-local expectation = {}
-expectation.__index = expectation
+return function(handle_mock_calls, m)
+  local o = {}
+  local call_specified = false
+  local calls = {}
+  local completed_calls = {}
+  local ignore_other_calls
 
-local function create(handle_mock_calls, m)
-  local o = {
-    _m = m,
-    _call_specified = false,
-    _calls = {},
-    _completed_calls = {},
-    _handle_mock_calls = handle_mock_calls
-  }
-
-  setmetatable(o, expectation)
-
-  return o
-end
-
-function expectation:and_will_return(...)
-  if not self._call_specified then
-    error('cannot set return value for an unspecified call', 2)
+  local function wrap(f)
+    return function(self, ...)
+      if not self or self == o then
+        return f(...)
+      else
+        return f(self, ...)
+      end
+    end
   end
 
-  self._calls[#self._calls]:set_return_values(...)
+  o.and_will_return = wrap(function(...)
+    if not call_specified then
+      error('cannot set return value for an unspecified call', 2)
+    end
 
-  return self
-end
+    calls[#calls]:set_return_values(...)
 
-function expectation:and_will_raise_error(...)
-  if not self._call_specified then
-    error('cannot set error for an unspecified call', 2)
-  end
+    return o
+  end)
 
-  self._calls[#self._calls]:set_error(...)
+  o.and_will_raise_error = wrap(function(...)
+    if not call_specified then
+      error('cannot set error for an unspecified call', 2)
+    end
 
-  return self
-end
+    calls[#calls]:set_error(...)
 
-function expectation:when(thunk)
-  if not self._call_specified then
-    error('incomplete expectation', 2)
-  end
+    return o
+  end)
 
-  local current_call_index = 1
+  o.when = wrap(function(thunk)
+    if not call_specified then
+      error('incomplete expectation', 2)
+    end
 
-  local function called(m, name, args)
-    local valid_function_found = false
-    local incomplete_expectation_found = false
+    local current_call_index = 1
 
-    for i = current_call_index, #self._calls do
-      local call = self._calls[i]
+    local function called(m, name, args)
+      local valid_function_found = false
+      local incomplete_expectation_found = false
 
-      if call:function_matches(m) then
-        valid_function_found = true
+      for i = current_call_index, #calls do
+        local call = calls[i]
 
-        if call:args_match(args) then
-          if call:has_fixed_order() and incomplete_expectation_found then
-            out_of_order_call_error(name, args, self._completed_calls, self._calls, 2)
+        if call:function_matches(m) then
+          valid_function_found = true
+
+          if call:args_match(args) then
+            if call:has_fixed_order() and incomplete_expectation_found then
+              out_of_order_call_error(name, args, completed_calls, calls, 2)
+            end
+
+            if call:has_fixed_order() then
+              current_call_index = i
+            end
+
+            table.remove(calls, i)
+
+            table.insert(completed_calls, CompletedCall(name, args))
+
+            if call:has_error() then
+              error(call:get_error())
+            end
+
+            return call:get_return_values()
           end
+        end
 
-          if call:has_fixed_order() then
-            current_call_index = i
-          end
-
-          table.remove(self._calls, i)
-
-          table.insert(self._completed_calls, CompletedCall(name, args))
-
-          if call:has_error() then
-            error(call:get_error())
-          end
-
-          return call:get_return_values()
+        if call:is_required() then
+          incomplete_expectation_found = true;
         end
       end
 
+      if not ignore_other_calls then
+        if not valid_function_found then
+          unexpected_call_error(name, args, completed_calls, calls, 2)
+        else
+          unexpected_args_error(name, args, completed_calls, calls, 2)
+        end
+      end
+    end
+
+    handle_mock_calls(called, thunk)
+
+    for _, call in pairs(calls) do
       if call:is_required() then
-        incomplete_expectation_found = true;
+        not_all_calls_occurred_error(completed_calls, calls, 2)
       end
     end
+  end)
 
-    if not self._ignore_other_calls then
-      if not valid_function_found then
-        unexpected_call_error(name, args, self._completed_calls, self._calls, 2)
-      else
-        unexpected_args_error(name, args, self._completed_calls, self._calls, 2)
-      end
+  o.after = wrap(function(thunk)
+    if not call_specified then
+      error('incomplete expectation', 2)
     end
-  end
 
-  self._handle_mock_calls(called, thunk)
+    o.when(thunk)
+  end)
 
-  for _, call in pairs(self._calls) do
-    if call:is_required() then
-      not_all_calls_occurred_error(self._completed_calls, self._calls, 2)
+  o.and_then = wrap(function(other)
+    for i, call in ipairs(other._calls()) do
+      if i == 1 then call:fix_order() end
+      table.insert(calls, call)
     end
-  end
-end
 
-function expectation:after(thunk)
-  if not self._call_specified then
-    error('incomplete expectation', 2)
-  end
+    return o
+  end)
 
-  self:when(thunk)
-end
+  o.and_also = wrap(function(other)
+    for _, call in ipairs(other._calls()) do
+      table.insert(calls, call)
+    end
 
-function expectation:and_then(other)
-  for i, call in ipairs(other._calls) do
-    if i == 1 then call:fix_order() end
-    table.insert(self._calls, call)
-  end
+    return o
+  end)
 
-  return self
-end
+  o.should_be_called_with_any_arguments = wrap(function()
+    if call_specified then
+      error('call already specified', 2)
+    end
 
-function expectation:and_also(other)
-  for _, call in ipairs(other._calls) do
-    table.insert(self._calls, call)
-  end
+    call_specified = true
+    table.insert(calls, ExpectedCall(m, { required = true, args = table.pack(), ignore_args = true }))
+    return o
+  end)
 
-  return self
-end
+  o.should_be_called_with = wrap(function(...)
+    if call_specified then
+      error('call already specified', 2)
+    end
 
-function expectation:should_be_called_with_any_arguments()
-  if self._call_specified then
-    error('call already specified', 2)
-  end
+    call_specified = true
+    table.insert(calls, ExpectedCall(m, { required = true, args = table.pack(...) }))
+    return o
+  end)
 
-  self._call_specified = true
-  table.insert(self._calls, ExpectedCall(self._m, { required = true, args = table.pack(), ignore_args = true }))
-  return self
-end
+  o.should_be_called = wrap(function()
+    if call_specified then
+      error('call already specified', 2)
+    end
 
-function expectation:should_be_called_with(...)
-  if self._call_specified then
-    error('call already specified', 2)
-  end
+    return o.should_be_called_with()
+  end)
 
-  self._call_specified = true
-  table.insert(self._calls, ExpectedCall(self._m, { required = true, args = table.pack(...) }))
-  return self
-end
+  o.may_be_called_with_any_arguments = wrap(function()
+    if call_specified then
+      error('call already specified', 2)
+    end
 
-function expectation:should_be_called()
-  if self._call_specified then
-    error('call already specified', 2)
-  end
+    call_specified = true
+    table.insert(calls, ExpectedCall(m, { required = false, ignore_args = true }))
+    return o
+  end)
 
-  return self:should_be_called_with()
-end
+  o.may_be_called_with = wrap(function(...)
+    if call_specified then
+      error('call already specified', 2)
+    end
 
-function expectation:may_be_called_with_any_arguments()
-  if self._call_specified then
-    error('call already specified', 2)
-  end
+    call_specified = true
+    table.insert(calls, ExpectedCall(m, { required = false, args = table.pack(...) }))
+    return o
+  end)
 
-  self._call_specified = true
-  table.insert(self._calls, ExpectedCall(self._m, { required = false, ignore_args = true }))
-  return self
-end
+  o.may_be_called = wrap(function()
+    if call_specified then
+      error('call already specified', 2)
+    end
 
-function expectation:may_be_called_with(...)
-  if self._call_specified then
-    error('call already specified', 2)
-  end
+    return o.may_be_called_with()
+  end)
 
-  self._call_specified = true
-  table.insert(self._calls, ExpectedCall(self._m, { required = false, args = table.pack(...) }))
-  return self
-end
+  o.multiple_times = wrap(function(times)
+    for i = 1, times - 1 do
+      table.insert(calls, calls[#calls])
+    end
 
-function expectation:may_be_called()
-  if self._call_specified then
-    error('call already specified', 2)
-  end
+    return o
+  end)
 
-  return self:may_be_called_with()
-end
+  o.and_other_calls_should_be_ignored = wrap(function()
+    ignore_other_calls = true
+    return o
+  end)
 
-function expectation:multiple_times(times)
-  for i = 1, times - 1 do
-    table.insert(self._calls, self._calls[#self._calls])
+  o._calls = function()
+    return calls
   end
 
-  return self
+  o.with_other_calls_ignored = o.and_other_calls_should_be_ignored
+
+  return o
 end
-
-function expectation:and_other_calls_should_be_ignored()
-  self._ignore_other_calls = true
-  return self
-end
-
-expectation.with_other_calls_ignored = expectation.and_other_calls_should_be_ignored
-
-return create
